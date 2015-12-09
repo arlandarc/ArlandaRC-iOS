49b271174281b8c96c8ea4ca534dd25135000c97
fabian <fabian.ostlund@devcode.se>
2015-12-09 17:25:28 +0100

    Initial Commit

---
+++ ArlandaRCTests/ArlandaRCTests.swift
@@ -0,0 +1,36 @@
+//
+//  ArlandaRCTests.swift
+//  ArlandaRCTests
+//
+//  Created by Fabian Östlund on 09/12/15.
+//  Copyright © 2015 Arlanda RC. All rights reserved.
+//
+
+import XCTest
+@testable import ArlandaRC
+
+class ArlandaRCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
